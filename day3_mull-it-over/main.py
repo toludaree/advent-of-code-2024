e8cee1c0512f68de632a7e55a43b5da1da761960
toludaree <isaactoluwani30@gmail.com>
2024-12-23 17:05:42 +0100

    day3 - solved

--- day3_mull-it-over/main.py
+++ day3_mull-it-over/main.py
@@ -1,19 +1,34 @@
 import re
 
-def sum_uncorrupted_instructions(instructions:list[tuple[int, int]]) -> int:
+def sum_instructions(memory:str, conditionals=False) -> int:
     """
     Return the sum of the multiplication of all the generated
     uncorrupted instructions.
-    """
-    return sum((ins[0]*ins[1]) for ins in instructions)
 
-def find_uncorrupted_instructions(memory:str) -> list[tuple[int, int]]:
+    If `conditionals` is True, only consider memory sections where mul
+    instructions are enabled.
+    """
+    return sum((ins[0]*ins[1])
+               for ins in find_instructions(memory, conditionals))
+
+def find_instructions(memory:str, conditionals=False) -> list[tuple[int, int]]:
     """
     Return all the uncorrupted instructions in `memory`.
+
+    If `conditionals` is True, only consider memory sections where mul
+    instructions are enabled.
+    """
+    return find_instructions_in_section(
+        determine_valid_memory_sections(memory, conditionals)
+    )
+
+def find_instructions_in_section(section:str) -> list[tuple[int, int]]:
+    """
+    Return all the uncorrupted mul instructions within `section`.
     """
     pattern = r"mul\((\d+),(\d+)\)"
     instructions = []
-    matches = re.finditer(pattern, memory)
+    matches = re.finditer(pattern, section, re.DOTALL)
     
     for match in matches:
         instructions.append(
@@ -24,6 +39,31 @@ def find_uncorrupted_instructions(memory:str) -> list[tuple[int, int]]:
         )
     
     return instructions
+
+def determine_valid_memory_sections(memory:str, conditionals=False) -> str:
+    """
+    If `conditionals` is True, only consider sections where instructions
+    are enabled. If False, return memory as-is.
+    
+    Instructions are enabled at the beginning and within "do()" blocks.
+    """
+    if not conditionals:
+        return memory
+    else:
+        if not "EOF" in memory:
+            memory_with_eof = memory + "EOF"
+            beginning_section_pattern = r".*?don't\(\)"
+            do_section_pattern = r"do\(\).*?don't\(\)|do\(\).*?EOF"
+            
+            beginning_section = [
+                re.search(beginning_section_pattern,
+                          memory_with_eof,
+                          re.DOTALL).group()
+            ]
+            do_sections = re.findall(do_section_pattern, memory_with_eof, re.DOTALL)
+            return "".join(beginning_section+do_sections)
+        else:
+            raise Exception("memory already contains EOF specifier; use another one.")
 
 def render_input() -> str:
     """
@@ -37,6 +77,7 @@ def render_input() -> str:
 
 if __name__ == "__main__":
     corrupted_memory = render_input()
-    uncorrupted_instructions = find_uncorrupted_instructions(corrupted_memory)
-    sum_ui = sum_uncorrupted_instructions(uncorrupted_instructions)
-    print(sum_ui)
+    sum_woc = sum_instructions(corrupted_memory, conditionals=False)
+    sum_wc = sum_instructions(corrupted_memory, conditionals=True)
+    print("Sum Without Conditionals:", sum_woc)
+    print("Sum With Conditionals:", sum_wc)
