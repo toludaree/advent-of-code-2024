734d6d84a38444dd0ec42293a73ffc94ee46a99c
toludaree <isaactoluwani30@gmail.com>
2024-12-25 03:11:24 +0100

    day4 - part one

--- day4_ceres_search/main.py
+++ day4_ceres_search/main.py
@@ -1,5 +1,88 @@
 from typing import Optional
 
+
+def number_of_xmas(M:list[list[str]]) -> int:
+    """
+    Given a matrix of characters, return the number of times
+    a valid search in `M` represents the string "XMAS".
+
+    Informally, a search in `M` is valid when the sequence of characters
+    is horizontal, vertical, diagonal, or backwards.
+
+    Formally, if the search space of `M` is given as S, then a member s of S is valid if:
+        - the length of s is 4
+        - the indices of s given as `((m1,n1), (m2,n2), (m3,n3), (m4,n4))` implies that either:
+            - (m1 = m2 = m3 = m4) and (n2-n1 = n3-n2 = n4-n3 = 1) or
+            - (m2-m1 = m3-m2 = m4-m3 = 1) and (n1 = n2 = n3 = n4) or
+            - (m2-m1 = m3-m2 = m4-m3 = 1) and (|n2-n1| = |n3-n2| = |n4-n3| = 1)
+    """
+    rows, columns = len(M), len(M[0])
+    if columns == 0:
+        return 0
+    else:
+        count = 0
+        valid_tuples = generate_valid_tuples_of_indices(rows=rows, columns=columns)
+        for tuples in valid_tuples:
+            if is_xmas(M, indices=tuples):
+                count += 1
+        return count
+
+def generate_valid_tuples_of_indices(rows:int, columns:int) -> set[tuple[tuple[int, int]]]:
+    """
+    Return a set S whose members are valid 4-tuple of indices of a matrix M.
+    M has `rows` rows and `columns` columns.
+
+    The set of all indices of the matrix M is
+    ```python
+    {(0,0), (0,1), ..., (0,columns-1),
+     (1,0), (1,1), ..., (1,columns-1),
+     ....., ....., ..., .............,
+     (rows-1, 1), (rows-1, 2), ..., (rows-1, columns-1)}
+     ```
+
+    A 4-tuple of indices, s = ((m1,n1), (m2,n2), (m3,n3), (m4,n4)) is valid
+    when any of the following is true:
+        - (m1 = m2 = m3 = m4) and (n2-n1 = n3-n2 = n4-n3 = 1)
+        - (m2-m1 = m3-m2 = m4-m3 = 1) and (n1 = n2 = n3 = n4)
+        - (m2-m1 = m3-m2 = m4-m3 = 1) and (|n2-n1| = |n3-n2| = |n4-n3| = 1)
+    """
+    valid_tuples = set()
+
+    for m in range(rows):
+        for n in range(columns):
+            valid_movements = generate_valid_movements(index=(m,n), rows=rows, columns=columns)
+            valid_tuples = valid_tuples.union(valid_movements)
+
+    return valid_tuples
+
+def generate_valid_movements(index:tuple[int, int], rows:int, columns:int) -> set[tuple[tuple[int, int]]]:
+    """
+    Return a set containing 4-tuple of indices, each element representing
+    a valid movement from `index`.
+
+    Valid movements are:
+        - left to right
+        - up to down
+        - left diagonal
+        - right diagonal
+    """
+    valid_movements = set()
+
+    ltr = move_left_to_right(index=index, columns=columns)
+    if ltr is not None:
+        valid_movements.add(ltr)
+    utd = move_up_to_down(index=index, rows=rows)
+    if utd is not None:
+        valid_movements.add(utd)
+    ld = move_left_diagonal(index=index, rows=rows, columns=columns)
+    if ld is not None:
+        valid_movements.add(ld)
+    rd = move_right_diagonal(index=index, rows=rows, columns=columns)
+    if rd is not None:
+        valid_movements.add(rd)
+    
+    return valid_movements
+
 def move_left_to_right(index:tuple[int, int], columns:int) -> Optional[tuple[tuple[int, int]]]:
     """
     Given an index of a matrix (m, n), return
@@ -63,3 +146,41 @@ def move_right_diagonal(index:tuple[int, int], rows:int, columns:int) -> Optiona
         return None
     else:
         return ((m,n), (m+1,n+1), (m+2,n+2), (m+3, n+3))
+
+def is_xmas(M:list[list[str]], indices:tuple[tuple[int, int]]) -> bool:
+    """
+    Given a matrix `M` with each row represented as a list of characters
+    and `indices` a 4-tuple of valid indexes of matrix `M`,
+
+    Return True if the location of `indices` in M represents the string
+    "XMAS" or "SAMX".
+    """
+    character_seq = [
+        M[indices[0][0]][indices[0][1]],
+        M[indices[1][0]][indices[1][1]],
+        M[indices[2][0]][indices[2][1]],
+        M[indices[3][0]][indices[3][1]]
+    ]
+    string = "".join(character_seq)
+    if string in ("XMAS", "SAMX"):
+        return True
+    return False
+
+def render_input() -> list[list[str]]:
+    """
+    Return the puzzle as a matrix M of characters. Each element
+    is a single-character long.
+
+    `M[m][n]` represents the element of the matrix at row m and column n.
+    """
+    M = []
+    with open("input.txt") as f:
+        for line in f.readlines():
+            row = list(line.strip())
+            M.append(row)
+    return M
+
+if __name__ == "__main__":
+    M = render_input()
+    count_xmas = number_of_xmas(M)
+    print(count_xmas)
--- day4_ceres_search/test.py
+++ day4_ceres_search/test.py
@@ -1,11 +1,24 @@
 import unittest
 from main import (
     move_left_to_right, move_up_to_down,
-    move_left_diagonal, move_right_diagonal
+    move_left_diagonal, move_right_diagonal,
+    generate_valid_movements,
+    generate_valid_tuples_of_indices,
+    is_xmas, number_of_xmas
 )
 
 
 class TestCeresSearch(unittest.TestCase):
+    M = [['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'],
+         ['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],
+         ['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],
+         ['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'],
+         ['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],
+         ['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'],
+         ['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'],
+         ['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'],
+         ['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],
+         ['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']]
 
     def test_move_left_to_right_1(self):
         self.assertEqual(
@@ -89,6 +102,124 @@ class TestCeresSearch(unittest.TestCase):
         self.assertEqual(
             move_right_diagonal(index=(2, 7), rows=10, columns=10),
             None
+        )
+
+    def test_generate_valid_movements_1(self):
+        self.assertEqual(
+            generate_valid_movements(index=(0,0), rows=7, columns=7),
+            {((0,0), (0,1), (0,2), (0,3)),
+             ((0,0), (1,0), (2,0), (3,0)),
+             ((0,0), (1,1), (2,2), (3,3))}
+        )
+
+    def test_generate_valid_movements_2(self):
+        self.assertEqual(
+            generate_valid_movements(index=(6,4), rows=10, columns=10),
+            {((6,4), (6,5), (6,6), (6,7)),
+             ((6,4), (7,4), (8,4), (9,4)),
+             ((6,4), (7,3), (8,2), (9,1)),
+             ((6,4), (7,5), (8,6), (9,7))}
+        )
+
+    def test_generate_valid_movements_3(self):
+        self.assertEqual(
+            generate_valid_movements(index=(4,9), rows=10, columns=10),
+            {((4,9), (5,9), (6,9), (7,9)),
+             ((4,9), (5,8), (6,7), (7,6))}
+        )
+
+    def test_generate_valid_movements_4(self):
+        self.assertEqual(
+            generate_valid_movements(index=(8,5), rows=10, columns=10),
+            {((8,5), (8,6), (8,7), (8,8))}
+        )
+
+    def test_generate_valid_movements_5(self):
+        self.assertEqual(
+            generate_valid_movements(index=(7,7), rows=10, columns=10),
+            set()
+        )
+
+    def test_generate_valid_tuples_of_indices_1(self):
+        self.assertEqual(
+            generate_valid_tuples_of_indices(rows=0, columns=0),
+            set()
+        )
+    
+    def test_generate_valid_tuples_of_indices_2(self):
+        self.assertEqual(
+            generate_valid_tuples_of_indices(rows=4, columns=4),
+            {((0,0), (0,1), (0,2), (0,3)),
+             ((0,0), (1,0), (2,0), (3,0)),
+             ((0,0), (1,1), (2,2), (3,3)),
+             ((0,1), (1,1), (2,1), (3,1)),
+             ((0,2), (1,2), (2,2), (3,2)),
+             ((0,3), (1,3), (2,3), (3,3)),
+             ((0,3), (1,2), (2,1), (3,0)),
+             ((1,0), (1,1), (1,2), (1,3)),
+             ((2,0), (2,1), (2,2), (2,3)),
+             ((3,0), (3,1), (3,2), (3,3))}
+        )
+
+    def test_is_xmas_1(self):
+        self.assertTrue(
+            is_xmas(self.M, ((0,5), (0,6), (0,7), (0,8)))
+        )
+    
+    def test_is_xmas_2(self):
+        self.assertFalse(
+            is_xmas(self.M, ((3,1), (3,2), (3,3), (3,4)))
+        )
+
+    def test_is_xmas_3(self):
+        self.assertTrue(
+            is_xmas(self.M, ((1,6), (2,6), (3,6), (4,6)))
+        )
+
+    def test_is_xmas_4(self):
+        self.assertFalse(
+            is_xmas(self.M, ((4,4), (5,4), (6,4), (7,4)))
+        )
+
+    def test_is_xmas_5(self):
+        self.assertTrue(
+            is_xmas(self.M, ((6,4), (7,3), (8,2), (9,1)))
+        )
+
+    def test_is_xmas_6(self):
+        self.assertFalse(
+            is_xmas(self.M, ((2,8), (3,7), (4,6), (5,5)))
+        )
+
+    def test_is_xmas_7(self):
+        self.assertTrue(
+            is_xmas(self.M, ((6,6), (7,7), (8,8), (9,9)))
+        )
+
+    def test_is_xmas_8(self):
+        self.assertFalse(
+            is_xmas(self.M, ((0,0), (1,1), (2,2), (3,3)))
+        )
+
+    def test_number_of_xmas_1(self):
+        self.assertEqual(
+            number_of_xmas([[]]),
+            0
+        )
+
+    def test_number_of_xmas_2(self):
+        self.assertEqual(
+            number_of_xmas([['S', 'A', 'M', 'X'],
+                            ['S', 'A', 'M', 'S'],
+                            ['X', 'A', 'M', 'S'],
+                            ['S', 'M', 'A', 'X']]),
+            3
+        )
+
+    def test_number_of_xmas_3(self):
+        self.assertEqual(
+            number_of_xmas(self.M),
+            18
         )
 
 
