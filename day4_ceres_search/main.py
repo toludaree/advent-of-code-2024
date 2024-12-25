ba339463121df30f23d419daf60b1bd87a62a57a
toludaree <isaactoluwani30@gmail.com>
2024-12-25 16:01:18 +0100

    day4 - solved

--- day4_ceres_search/main.py
+++ day4_ceres_search/main.py
@@ -1,36 +1,52 @@
 from typing import Optional
 
 
-def number_of_xmas(M:list[list[str]]) -> int:
+def number_of_xmas(M:list[list[str]], x_shape=False) -> int:
     """
-    Given a matrix of characters, return the number of times
-    a valid search in `M` represents the string "XMAS".
+    Given a matrix of characters:
+    
+    If `x_shape` is False, return the number of times a valid search in `M`
+    represents the string "XMAS".
+
+    If `x_shape` is True, return the number of times a valid search in `M`
+    represents two strings, each "MAS", in the shape of an `X`.
 
     Informally, a search in `M` is valid when the sequence of characters
     is horizontal, vertical, diagonal, or backwards.
 
-    Formally, if the search space of `M` is given as S, then a member s of S is valid if:
-        - the length of s is 4
-        - the indices of s given as `((m1,n1), (m2,n2), (m3,n3), (m4,n4))` implies that either:
-            - (m1 = m2 = m3 = m4) and (n2-n1 = n3-n2 = n4-n3 = 1) or
-            - (m2-m1 = m3-m2 = m4-m3 = 1) and (n1 = n2 = n3 = n4) or
-            - (m2-m1 = m3-m2 = m4-m3 = 1) and (|n2-n1| = |n3-n2| = |n4-n3| = 1)
+    If the search space of `M` is given as a set of all tuples of indices of 'M` called S,
+    then s in S is valid if:
+
+    ## x_shape is False
+    - the length of s is 4
+    - s = `((m1,n1), (m2,n2), (m3,n3), (m4,n4))` implies that either
+        - (m1 = m2 = m3 = m4) and (n2-n1 = n3-n2 = n4-n3 = 1)
+        - (m2-m1 = m3-m2 = m4-m3 = 1) and (n1 = n2 = n3 = n4)
+        - (m2-m1 = m3-m2 = m4-m3 = 1) and (|n2-n1| = |n3-n2| = |n4-n3| = 1)
+    ## x_shape is True
+    - the length of s is 6
+    - s = `((m1,n1), (m2,n2), (m3,n3), (m4,n4), (m5,n5), (m6,n6))` implies that
+        - (m2,n2) = (m5,n5) = (m,n)
+        - (m1,n1) = (m-1,n-1)
+        - (m3,n3) = (m+1,n+1)
+        - (m4,n4) = (m-1,n+1)
+        - (m6,n6) = (m+1,n-1)
     """
     rows, columns = len(M), len(M[0])
     if columns == 0:
         return 0
     else:
         count = 0
-        valid_tuples = generate_valid_tuples_of_indices(rows=rows, columns=columns)
+        valid_tuples = generate_valid_tuples_of_indices(rows=rows, columns=columns, x_shape=x_shape)
         for tuples in valid_tuples:
-            if is_xmas(M, indices=tuples):
+            if is_xmas(M, indices=tuples, x_shape=x_shape):
                 count += 1
         return count
 
-def generate_valid_tuples_of_indices(rows:int, columns:int) -> set[tuple[tuple[int, int]]]:
+def generate_valid_tuples_of_indices(rows:int, columns:int, x_shape=False) -> set[tuple[tuple[int, int]]]:
     """
-    Return a set S whose members are valid 4-tuple of indices of a matrix M.
-    M has `rows` rows and `columns` columns.
+    Return a set S whose members are valid 4|6-tuple of indices of a matrix M
+    depending on `x_shape`. M has `rows` rows and `columns` columns.
 
     The set of all indices of the matrix M is
     ```python
@@ -40,58 +56,62 @@ def generate_valid_tuples_of_indices(rows:int, columns:int) -> set[tuple[tuple[i
      (rows-1, 1), (rows-1, 2), ..., (rows-1, columns-1)}
      ```
 
-    A 4-tuple of indices, s = ((m1,n1), (m2,n2), (m3,n3), (m4,n4)) is valid
-    when any of the following is true:
-        - (m1 = m2 = m3 = m4) and (n2-n1 = n3-n2 = n4-n3 = 1)
-        - (m2-m1 = m3-m2 = m4-m3 = 1) and (n1 = n2 = n3 = n4)
-        - (m2-m1 = m3-m2 = m4-m3 = 1) and (|n2-n1| = |n3-n2| = |n4-n3| = 1)
+    The definition of validity for any member of set S is given in the
+    docstring for `number_of_xmas`.
     """
     valid_tuples = set()
 
     for m in range(rows):
         for n in range(columns):
-            valid_movements = generate_valid_movements(index=(m,n), rows=rows, columns=columns)
+            valid_movements = generate_valid_movements(index=(m,n), rows=rows, columns=columns, x_shape=x_shape)
             valid_tuples = valid_tuples.union(valid_movements)
 
     return valid_tuples
 
-def generate_valid_movements(index:tuple[int, int], rows:int, columns:int) -> set[tuple[tuple[int, int]]]:
+def generate_valid_movements(index:tuple[int, int], rows:int, columns:int, x_shape=False) -> set[tuple[tuple[int, int]]]:
     """
-    Return a set containing 4-tuple of indices, each element representing
-    a valid movement from `index`.
+    Return a valid movement from a reference point `index` depending on `x_shape`.
 
-    Valid movements are:
-        - left to right
-        - up to down
-        - left diagonal
-        - right diagonal
+    If `x_shape` is False, return a set containing 4-tuple of indices, each element representing
+    a valid search of a matrix M.
+
+    If `x_shape` is True, return a singleton set containing a 6-tuple of indices, representing
+    a valid search of a matrix M.
+
+    M has `rows` rows and `columns` columns. The definition of a valid search
+    is given in the docstring of `number_of_xmas`.
     """
     valid_movements = set()
 
-    ltr = move_left_to_right(index=index, columns=columns)
-    if ltr is not None:
-        valid_movements.add(ltr)
-    utd = move_up_to_down(index=index, rows=rows)
-    if utd is not None:
-        valid_movements.add(utd)
-    ld = move_left_diagonal(index=index, rows=rows, columns=columns)
-    if ld is not None:
-        valid_movements.add(ld)
-    rd = move_right_diagonal(index=index, rows=rows, columns=columns)
-    if rd is not None:
-        valid_movements.add(rd)
+    if not x_shape:
+        ltr = move_left_to_right(index=index, columns=columns)
+        if ltr is not None:
+            valid_movements.add(ltr)
+        utd = move_up_to_down(index=index, rows=rows)
+        if utd is not None:
+            valid_movements.add(utd)
+        ld = move_left_diagonal(index=index, rows=rows, columns=columns)
+        if ld is not None:
+            valid_movements.add(ld)
+        rd = move_right_diagonal(index=index, rows=rows, columns=columns)
+        if rd is not None:
+            valid_movements.add(rd)
+    else:
+        mx = move_x(index=index, rows=rows, columns=columns)
+        if mx is not None:
+            valid_movements.add(mx)
     
     return valid_movements
 
 def move_left_to_right(index:tuple[int, int], columns:int) -> Optional[tuple[tuple[int, int]]]:
     """
-    Given an index of a matrix (m, n), return
+    Given an index (m, n) of a matrix M, return
         ((m,n), (m,n+1), (m,n+2), (m,n+3))
-    
-    `columns` is the number of columns in the matrix from which (m,n)
-    is drawn.
 
-    If n+3 >= `columns`, return None.
+    or None if
+        n+3 >= `columns`
+    
+    `columns` represents the number of columns of M.
     """
     m, n = index
     if n+3 >= columns:
@@ -101,13 +121,13 @@ def move_left_to_right(index:tuple[int, int], columns:int) -> Optional[tuple[tup
     
 def move_up_to_down(index:tuple[int, int], rows:int) -> Optional[tuple[tuple[int, int]]]:
     """
-    Given an index of a matrix (m, n), return
+    Given an index (m, n) of a matrix M, return
         ((m,n), (m+1,n), (m+2,n), (m+3,n))
 
-    `rows` is the number of rows in the matrix from which (m,n)
-    is drawn.
+    or None if
+        m+3 >= `rows`
 
-    If m+3 >= `rows`, return None.
+    `rows` represents the number of rows of M.
     """
     m, n = index
     if m+3 >= rows:
@@ -117,13 +137,13 @@ def move_up_to_down(index:tuple[int, int], rows:int) -> Optional[tuple[tuple[int
     
 def move_left_diagonal(index:tuple[int, int], rows:int, columns:int) -> Optional[tuple[tuple[int, int]]]:
     """
-    Given an index of a matrix (m, n), return
+    Given an index (m, n) of a matrix M, return
         ((m,n), (m+1,n-1), (m+2,n-2), (m+3,n-3))
 
-    `rows` and `columns` represents the number of rows and columns
-    in the matrix from which (m,n) is drawn.
+    or None if
+        (m+3 >= `rows`) or (n-3 < 0)
 
-    If m+3 >= `rows` or n-3 < 0, return None.
+    `rows` and `columns` represents the number of rows and columns of M.
     """
     m, n = index
     if (m+3 >= rows) or (n-3 < 0):
@@ -133,13 +153,13 @@ def move_left_diagonal(index:tuple[int, int], rows:int, columns:int) -> Optional
     
 def move_right_diagonal(index:tuple[int, int], rows:int, columns:int) -> Optional[tuple[tuple[int, int]]]:
     """
-    Given an index of a matrix (m, n), return
+    Given an index (m, n) of a matrix M, return
         ((m,n), (m+1,n+1), (m+2,n+2), (m+3,n+3))
 
-    `rows` and `columns` represents the number of rows and columns
-    in the matrix from which (m,n) is drawn.
+    or None if
+        (m+3 >= `rows`) or (n+3 >= `columns`)
 
-    If m+3 >= `rows` or n+3 >= `columns`, return None.
+    `rows` and `columns` represents the number of rows and columns of M.
     """
     m, n = index
     if (m+3 >= rows) or (n+3 >= columns):
@@ -147,24 +167,58 @@ def move_right_diagonal(index:tuple[int, int], rows:int, columns:int) -> Optiona
     else:
         return ((m,n), (m+1,n+1), (m+2,n+2), (m+3, n+3))
 
-def is_xmas(M:list[list[str]], indices:tuple[tuple[int, int]]) -> bool:
+def move_x(index:tuple[int, int], rows:int, columns:int) -> Optional[tuple[tuple[int, int]]]:
+    """
+    Given an index (m, n) of a matrix M, return
+        ((m-1,n-1), (m,n), (m+1,n+1), (m-1,n+1), (m,n), (m+1,n-1))
+
+    or None if
+        (m-1 < 0) or (n-1 < 0) or (m+1 >= `rows`) or (n+1 >= `columns`)
+
+    `rows` and `columns` represents the number of rows and columns of M.
+    """
+    m, n = index
+    if (m-1 < 0) or (n-1 < 0) or (m+1 >= rows) or (n+1 >= columns):
+        return None
+    else:
+        return ((m-1,n-1), (m,n), (m+1,n+1), (m-1,n+1), (m,n), (m+1,n-1))
+
+def is_xmas(M:list[list[str]], indices:tuple[tuple[int, int]], x_shape=False) -> bool:
     """
     Given a matrix `M` with each row represented as a list of characters
-    and `indices` a 4-tuple of valid indexes of matrix `M`,
+    and `indices` a 4|6-tuple of valid indexes of matrix `M`,
 
-    Return True if the location of `indices` in M represents the string
-    "XMAS" or "SAMX".
+    If `x_shape` is False, return True if the location of `indices`
+    in M represents the string "XMAS" or "SAMX".
+
+    If `x_shape` is True, return True if the location of indices in M represents
+    either one of the following strings:
+        ("MASMAS", "MASSAM", "SAMMAS", "SAMSAM")
     """
-    character_seq = [
-        M[indices[0][0]][indices[0][1]],
-        M[indices[1][0]][indices[1][1]],
-        M[indices[2][0]][indices[2][1]],
-        M[indices[3][0]][indices[3][1]]
-    ]
-    string = "".join(character_seq)
-    if string in ("XMAS", "SAMX"):
-        return True
-    return False
+    if not x_shape:
+        character_seq = [
+            M[indices[0][0]][indices[0][1]],
+            M[indices[1][0]][indices[1][1]],
+            M[indices[2][0]][indices[2][1]],
+            M[indices[3][0]][indices[3][1]]
+        ]
+        string = "".join(character_seq)
+        if string in ("XMAS", "SAMX"):
+            return True
+        return False
+    else:
+        character_seq = [
+            M[indices[0][0]][indices[0][1]],
+            M[indices[1][0]][indices[1][1]],
+            M[indices[2][0]][indices[2][1]],
+            M[indices[3][0]][indices[3][1]],
+            M[indices[4][0]][indices[4][1]],
+            M[indices[5][0]][indices[5][1]]
+        ]
+        string = "".join(character_seq)
+        if string in ("MASMAS", "MASSAM", "SAMMAS", "SAMSAM"):
+            return True
+        return False
 
 def render_input() -> list[list[str]]:
     """
@@ -182,5 +236,7 @@ def render_input() -> list[list[str]]:
 
 if __name__ == "__main__":
     M = render_input()
-    count_xmas = number_of_xmas(M)
-    print(count_xmas)
+    count_xmas = number_of_xmas(M, x_shape=False)
+    count_mas_x = number_of_xmas(M, x_shape=True)
+    print("Number of XMAS:", count_xmas)
+    print("Number of X-MAS:", count_mas_x)
--- day4_ceres_search/test.py
+++ day4_ceres_search/test.py
@@ -2,6 +2,7 @@ import unittest
 from main import (
     move_left_to_right, move_up_to_down,
     move_left_diagonal, move_right_diagonal,
+    move_x,
     generate_valid_movements,
     generate_valid_tuples_of_indices,
     is_xmas, number_of_xmas
@@ -33,15 +34,13 @@ class TestCeresSearch(unittest.TestCase):
         )
 
     def test_move_left_to_right_3(self):
-        self.assertEqual(
-            move_left_to_right(index=(3, 4), columns=7),
-            None
+        self.assertIsNone(
+            move_left_to_right(index=(3, 4), columns=7)
         )
 
     def test_move_left_to_right_4(self):
-        self.assertEqual(
-            move_left_to_right(index=(4, 6), columns=7),
-            None
+        self.assertIsNone(
+            move_left_to_right(index=(4, 6), columns=7)
         )
 
     def test_up_to_down_1(self):
@@ -57,15 +56,13 @@ class TestCeresSearch(unittest.TestCase):
         )
 
     def test_up_to_down_3(self):
-        self.assertEqual(
-            move_up_to_down(index=(3, 4), rows=5),
-            None
+        self.assertIsNone(
+            move_up_to_down(index=(3, 4), rows=5)
         )
 
     def test_up_to_down_4(self):
-        self.assertEqual(
-            move_up_to_down(index=(7, 6), rows=8),
-            None
+        self.assertIsNone(
+            move_up_to_down(index=(7, 6), rows=8)
         )
 
     def test_move_left_diagonal_1(self):
@@ -75,15 +72,13 @@ class TestCeresSearch(unittest.TestCase):
         )
 
     def test_move_left_diagonal_2(self):
-        self.assertEqual(
-            move_left_diagonal(index=(9, 2), rows=10, columns=10),
-            None
+        self.assertIsNone(
+            move_left_diagonal(index=(9, 2), rows=10, columns=10)
         )
 
     def test_move_left_diagonal_3(self):
-        self.assertEqual(
-            move_left_diagonal(index=(4, 2), rows=10, columns=7),
-            None
+        self.assertIsNone(
+            move_left_diagonal(index=(4, 2), rows=10, columns=7)
         )
 
     def test_move_right_diagonal_1(self):
@@ -93,20 +88,29 @@ class TestCeresSearch(unittest.TestCase):
         )
 
     def test_move_right_diagonal_2(self):
-        self.assertEqual(
-            move_right_diagonal(index=(9, 2), rows=10, columns=10),
-            None
+        self.assertIsNone(
+            move_right_diagonal(index=(9, 2), rows=10, columns=10)
         )
 
     def test_move_right_diagonal_3(self):
+        self.assertIsNone(
+            move_right_diagonal(index=(2, 7), rows=10, columns=10)
+        )
+
+    def test_move_x_1(self):
         self.assertEqual(
-            move_right_diagonal(index=(2, 7), rows=10, columns=10),
-            None
+            move_x(index=(7,1), rows=10, columns=10),
+            ((6,0), (7,1), (8,2), (6,2), (7,1), (8,0))
+        )
+
+    def test_move_x_2(self):
+        self.assertIsNone(
+            move_x(index=(0,4), rows=10, columns=10)
         )
 
     def test_generate_valid_movements_1(self):
         self.assertEqual(
-            generate_valid_movements(index=(0,0), rows=7, columns=7),
+            generate_valid_movements(index=(0,0), rows=7, columns=7, x_shape=False),
             {((0,0), (0,1), (0,2), (0,3)),
              ((0,0), (1,0), (2,0), (3,0)),
              ((0,0), (1,1), (2,2), (3,3))}
@@ -123,20 +127,44 @@ class TestCeresSearch(unittest.TestCase):
 
     def test_generate_valid_movements_3(self):
         self.assertEqual(
-            generate_valid_movements(index=(4,9), rows=10, columns=10),
+            generate_valid_movements(index=(4,9), rows=10, columns=10, x_shape=False),
             {((4,9), (5,9), (6,9), (7,9)),
              ((4,9), (5,8), (6,7), (7,6))}
         )
 
     def test_generate_valid_movements_4(self):
         self.assertEqual(
-            generate_valid_movements(index=(8,5), rows=10, columns=10),
+            generate_valid_movements(index=(8,5), rows=10, columns=10, x_shape=False),
             {((8,5), (8,6), (8,7), (8,8))}
         )
 
     def test_generate_valid_movements_5(self):
         self.assertEqual(
             generate_valid_movements(index=(7,7), rows=10, columns=10),
+            set()
+        )
+
+    def test_generate_valid_movements_6(self):
+        self.assertEqual(
+            generate_valid_movements(index=(8,5), rows=10, columns=10, x_shape=True),
+            {((7,4), (8,5), (9,6), (7,6), (8,5), (9,4))}
+        )
+    
+    def test_generate_valid_movements_6(self):
+        self.assertEqual(
+            generate_valid_movements(index=(0,2), rows=10, columns=10, x_shape=True),
+            set()
+        )
+
+    def test_generate_valid_movements_7(self):
+        self.assertEqual(
+            generate_valid_movements(index=(4,9), rows=10, columns=10, x_shape=True),
+            set()
+        )
+
+    def test_generate_valid_movements_8(self):
+        self.assertEqual(
+            generate_valid_movements(index=(9,5), rows=10, columns=10, x_shape=True),
             set()
         )
 
@@ -148,7 +176,7 @@ class TestCeresSearch(unittest.TestCase):
     
     def test_generate_valid_tuples_of_indices_2(self):
         self.assertEqual(
-            generate_valid_tuples_of_indices(rows=4, columns=4),
+            generate_valid_tuples_of_indices(rows=4, columns=4, x_shape=False),
             {((0,0), (0,1), (0,2), (0,3)),
              ((0,0), (1,0), (2,0), (3,0)),
              ((0,0), (1,1), (2,2), (3,3)),
@@ -161,6 +189,15 @@ class TestCeresSearch(unittest.TestCase):
              ((3,0), (3,1), (3,2), (3,3))}
         )
 
+    def test_generate_valid_tuples_of_indices_3(self):
+        self.assertEqual(
+            generate_valid_tuples_of_indices(rows=4, columns=4, x_shape=True),
+            {((0,0), (1,1), (2,2), (0,2), (1,1), (2,0)),
+             ((0,1), (1,2), (2,3), (0,3), (1,2), (2,1)),
+             ((1,0), (2,1), (3,2), (1,2), (2,1), (3,0)),
+             ((1,1), (2,2), (3,3), (1,3), (2,2), (3,1))}
+        )
+
     def test_is_xmas_1(self):
         self.assertTrue(
             is_xmas(self.M, ((0,5), (0,6), (0,7), (0,8)))
@@ -168,12 +205,12 @@ class TestCeresSearch(unittest.TestCase):
     
     def test_is_xmas_2(self):
         self.assertFalse(
-            is_xmas(self.M, ((3,1), (3,2), (3,3), (3,4)))
+            is_xmas(self.M, ((3,1), (3,2), (3,3), (3,4)), x_shape=False)
         )
 
     def test_is_xmas_3(self):
         self.assertTrue(
-            is_xmas(self.M, ((1,6), (2,6), (3,6), (4,6)))
+            is_xmas(self.M, ((1,6), (2,6), (3,6), (4,6)), x_shape=False)
         )
 
     def test_is_xmas_4(self):
@@ -183,7 +220,7 @@ class TestCeresSearch(unittest.TestCase):
 
     def test_is_xmas_5(self):
         self.assertTrue(
-            is_xmas(self.M, ((6,4), (7,3), (8,2), (9,1)))
+            is_xmas(self.M, ((6,4), (7,3), (8,2), (9,1)), x_shape=False)
         )
 
     def test_is_xmas_6(self):
@@ -201,6 +238,16 @@ class TestCeresSearch(unittest.TestCase):
             is_xmas(self.M, ((0,0), (1,1), (2,2), (3,3)))
         )
 
+    def test_is_xmas_9(self):
+        self.assertTrue(
+            is_xmas(self.M, ((2,1), (3,2), (4,3), (2,3), (3,2), (4,1)), x_shape=True)
+        )
+
+    def test_is_xmas_10(self):
+        self.assertFalse(
+            is_xmas(self.M, ((5,6), (6,7), (7,8), (5,8), (6,7), (7,6)), x_shape=True)
+        )
+
     def test_number_of_xmas_1(self):
         self.assertEqual(
             number_of_xmas([[]]),
@@ -212,14 +259,29 @@ class TestCeresSearch(unittest.TestCase):
             number_of_xmas([['S', 'A', 'M', 'X'],
                             ['S', 'A', 'M', 'S'],
                             ['X', 'A', 'M', 'S'],
-                            ['S', 'M', 'A', 'X']]),
+                            ['S', 'M', 'A', 'X']], x_shape=False),
             3
         )
 
     def test_number_of_xmas_3(self):
         self.assertEqual(
-            number_of_xmas(self.M),
+            number_of_xmas([['S', 'A', 'M', 'X'],
+                            ['S', 'A', 'M', 'S'],
+                            ['S', 'A', 'M', 'S'],
+                            ['S', 'M', 'M', 'X']], x_shape=True),
+            2
+        )
+
+    def test_number_of_xmas_4(self):
+        self.assertEqual(
+            number_of_xmas(self.M, x_shape=False),
             18
+        )
+
+    def test_number_of_xmas_5(self):
+        self.assertEqual(
+            number_of_xmas(self.M, x_shape=True),
+            9
         )
 
 
