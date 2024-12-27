d6464f0569859399797bad4fb99ab81c9e0dc03d
toludaree <isaactoluwani30@gmail.com>
2024-12-27 21:22:43 +0100

    day 5 - solved

--- day5_print_queue/main.py
+++ day5_print_queue/main.py
@@ -1,31 +1,50 @@
 from math import floor
 
-def sum_middle_correctly_ordered_updates(rules:set[tuple[int, int]], updates:list[list[int]]) -> int:
+def sum_middle_correctly_ordered_updates(rules:set[tuple[int, int]], updates:list[list[int]], incorrect=False) -> int:
     """
     Return the sum of the middle page in the updates that are
-    correctly ordered.
+    correctly ordered if `incorrect` is False.
+    
+    If `incorrect` is True, order the incorrectly ordered
+    updates according to `rules` and return the sum of their
+    middle pages.
 
     A correctly ordered update is one in which the order respects the
     page ordering rule.
     """
     return sum(
         update[floor(len(update)/2)]
-        for update in find_correctly_ordered_updates(rules, updates)
+        for update in find_correctly_ordered_updates(rules, updates, incorrect)
     )
 
-def find_correctly_ordered_updates(rules:set[tuple[int, int]], updates:list[list[int]]) -> list[list[int]]:
+def find_correctly_ordered_updates(rules:set[tuple[int, int]], updates:list[list[int]], incorrect=False) -> list[list[int]]:
     """
-    Find the page updates in `updates` that are correctly ordered.
+    Find the page updates in `updates` that are correctly ordered if
+    `incorrect` is False.
+
+    If `incorrect` is True,
+        - find the updates that are incorrectly ordered,
+        - correctly order them, and
+        - return the correctly ordered version.
     """
     correctly_ordered_updates = []
+    incorrectly_ordered_updates = []
 
     for update in updates:
         if is_correctly_ordered(update, rules):
             correctly_ordered_updates.append(update)
+        else:
+            incorrectly_ordered_updates.append(update)
 
-    return correctly_ordered_updates
+    if not incorrect:
+        return correctly_ordered_updates
+    else:
+        return [
+            correctly_order(update, rules)
+            for update in incorrectly_ordered_updates
+        ]
 
-def is_correctly_ordered(update:list[int], rules:set[tuple[int, int]]):
+def is_correctly_ordered(update:list[int], rules:set[tuple[int, int]]) -> bool:
     """
     Return True if update is correctly ordered.
     """
@@ -34,6 +53,24 @@ def is_correctly_ordered(update:list[int], rules:set[tuple[int, int]]):
             if (update[page_i], page) not in rules:
                 return False
     return True
+
+def correctly_order(update:list[int], rules:set[tuple[int, int]]) -> list[int]:
+    """
+    Correctly order `update` according to `rules`.
+    """
+    for i in range(len(update)-2, -1, -1):
+        while (i+1 < len(update)) and (not (comes_before(update[i], update[i+1], rules))):
+            update[i], update[i+1] = update[i+1], update[i]
+            i += 1
+    return update
+
+def comes_before(page1:int, page2:int, rules:set[tuple[int, int]]) -> bool:
+    """
+    Return True if `page1` comes before `page2` according to `rules`. 
+    """
+    if (page1, page2) in rules:
+        return True
+    return False
 
 def render_input() -> tuple[set[tuple[int, int]], list[list[int]]]:
     """
@@ -59,5 +96,20 @@ def render_input() -> tuple[set[tuple[int, int]], list[list[int]]]:
 
 
 if __name__ == "__main__":
-    page_ordering_rules, page_updates = render_input()
-    print(sum_middle_correctly_ordered_updates(page_ordering_rules, page_updates))
+    # page_ordering_rules, page_updates = render_input()
+    PAGE_ORDERING_RULES = {
+        (47, 53), (97, 13), (97, 61), (97, 47), (75, 29),
+        (61, 13), (75, 53), (29, 13), (97, 29), (53, 29),
+        (61, 53), (97, 53), (61, 29), (47, 13), (75, 47),
+        (97, 75), (47, 61), (75, 61), (47, 29), (75, 13),
+        (53, 13)
+    }
+    PAGES_TO_PRODUCE = [
+        [75, 47, 61, 53, 29],
+        [97, 61, 53, 29, 13],
+        [75, 29, 13],
+        [75, 97, 47, 61, 53],
+        [61, 13, 29],
+        [97, 13, 75, 29, 47]
+    ]
+    # print(sum_middle_correctly_ordered_updates(page_ordering_rules, page_updates, incorrect=True))
--- day5_print_queue/test.py
+++ day5_print_queue/test.py
@@ -2,35 +2,38 @@ import unittest
 from main import (
     sum_middle_correctly_ordered_updates,
     find_correctly_ordered_updates,
-    is_correctly_ordered
+    is_correctly_ordered,
+    correctly_order,
+    comes_before
 )
 
 
 class TestPrintQueue(unittest.TestCase):
-    PAGE_ORDERING_RULES_1 = {
-        (29, 11), (15, 11), (15, 29),
-        (75, 11), (75, 29), (15, 75)
-    }
-    PAGES_TO_PRODUCE_1 = [
-        [15, 75, 29],
-        [15, 29, 75],
-        [75, 29, 11]
-    ]
-    PAGE_ORDERING_RULES_2 = {
-        (47, 53), (97, 13), (97, 61), (97, 47), (75, 29),
-        (61, 13), (75, 53), (29, 13), (97, 29), (53, 29),
-        (61, 53), (97, 53), (61, 29), (47, 13), (75, 47),
-        (97, 75), (47, 61), (75, 61), (47, 29), (75, 13),
-        (53, 13)
-    }
-    PAGES_TO_PRODUCE_2 = [
-        [75, 47, 61, 53, 29],
-        [97, 61, 53, 29, 13],
-        [75, 29, 13],
-        [75, 97, 47, 61, 53],
-        [61, 13, 29],
-        [97, 13, 75, 29, 47]
-    ]
+    def setUp(self):
+        self.PAGE_ORDERING_RULES_1 = {
+            (29, 11), (15, 11), (15, 29),
+            (75, 11), (75, 29), (15, 75)
+        }
+        self.PAGES_TO_PRODUCE_1 = [
+            [15, 75, 29],
+            [15, 29, 75],
+            [75, 29, 11]
+        ]
+        self.PAGE_ORDERING_RULES_2 = {
+            (47, 53), (97, 13), (97, 61), (97, 47), (75, 29),
+            (61, 13), (75, 53), (29, 13), (97, 29), (53, 29),
+            (61, 53), (97, 53), (61, 29), (47, 13), (75, 47),
+            (97, 75), (47, 61), (75, 61), (47, 29), (75, 13),
+            (53, 13)
+        }
+        self.PAGES_TO_PRODUCE_2 = [
+            [75, 47, 61, 53, 29],
+            [97, 61, 53, 29, 13],
+            [75, 29, 13],
+            [75, 97, 47, 61, 53],
+            [61, 13, 29],
+            [97, 13, 75, 29, 47]
+        ]
 
     def test_sum_middle_correctly_ordered_updates_1(self):
         self.assertEqual(
@@ -45,9 +48,30 @@ class TestPrintQueue(unittest.TestCase):
         self.assertEqual(
             sum_middle_correctly_ordered_updates(
                 self.PAGE_ORDERING_RULES_2,
-                self.PAGES_TO_PRODUCE_2
+                self.PAGES_TO_PRODUCE_2,
+                incorrect=False
             ),
             143
+        )
+
+    def test_sum_middle_correctly_ordered_updates_3(self):
+        self.assertEqual(
+            sum_middle_correctly_ordered_updates(
+                self.PAGE_ORDERING_RULES_1,
+                self.PAGES_TO_PRODUCE_1,
+                incorrect=True
+            ),
+            75
+        )
+
+    def test_sum_middle_correctly_ordered_updates_4(self):
+        self.assertEqual(
+            sum_middle_correctly_ordered_updates(
+                self.PAGE_ORDERING_RULES_2,
+                self.PAGES_TO_PRODUCE_2,
+                incorrect=True
+            ),
+            123
         )
 
     def test_find_correctly_ordered_updates_1(self):
@@ -63,9 +87,30 @@ class TestPrintQueue(unittest.TestCase):
         self.assertEqual(
             find_correctly_ordered_updates(
                 self.PAGE_ORDERING_RULES_2,
-                self.PAGES_TO_PRODUCE_2
+                self.PAGES_TO_PRODUCE_2,
+                incorrect=False
             ),
             [[75, 47, 61, 53, 29], [97, 61, 53, 29, 13], [75, 29, 13]]
+        )
+
+    def test_find_correctly_ordered_updates_3(self):
+        self.assertEqual(
+            find_correctly_ordered_updates(
+                self.PAGE_ORDERING_RULES_1,
+                self.PAGES_TO_PRODUCE_1,
+                incorrect=True
+            ),
+            [[15, 75, 29]]
+        )
+
+    def test_find_correctly_ordered_updates_4(self):
+        self.assertEqual(
+            find_correctly_ordered_updates(
+                self.PAGE_ORDERING_RULES_2,
+                self.PAGES_TO_PRODUCE_2,
+                incorrect=True
+            ),
+            [[97, 75, 47, 61, 53], [61, 29, 13], [97, 75, 47, 29, 13]]
         )
 
     def test_is_correctly_ordered_1(self):
@@ -96,6 +141,65 @@ class TestPrintQueue(unittest.TestCase):
         self.assertFalse(
             is_correctly_ordered(
                 [61, 13, 29],
+                self.PAGE_ORDERING_RULES_2
+            )
+        )
+
+    def test_correctly_order_1(self):
+        self.assertEqual(
+            correctly_order(
+                [15, 29, 75],
+                self.PAGE_ORDERING_RULES_1
+            ),
+            [15, 75, 29]
+        )
+
+    def test_correctly_order_2(self):
+        self.assertEqual(
+            correctly_order(
+                [97, 13, 75, 29, 47],
+                self.PAGE_ORDERING_RULES_2
+            ),
+            [97, 75, 47, 29, 13]
+        )
+
+    def test_correctly_order_3(self):
+        self.assertEqual(
+            correctly_order(
+                [97, 61, 53, 29, 13],
+                self.PAGE_ORDERING_RULES_2
+            ),
+            [97, 61, 53, 29, 13]
+        )
+
+    def test_comes_before_1(self):
+        self.assertTrue(
+            comes_before(
+                75, 29,
+                self.PAGE_ORDERING_RULES_1
+            )
+        )
+
+    def test_comes_before_2(self):
+        self.assertFalse(
+            comes_before(
+                11, 15,
+                self.PAGE_ORDERING_RULES_1
+            )
+        )
+
+    def test_comes_before_3(self):
+        self.assertTrue(
+            comes_before(
+                61, 29,
+                self.PAGE_ORDERING_RULES_2
+            )
+        )
+
+    def test_comes_before_4(self):
+        self.assertFalse(
+            comes_before(
+                29, 53,
                 self.PAGE_ORDERING_RULES_2
             )
         )
