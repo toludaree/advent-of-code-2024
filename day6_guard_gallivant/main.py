a78a57f3885083d5e1cf6bfb8a95efa938ba8419
toludaree <isaactoluwani30@gmail.com>
2025-01-08 20:12:57 +0100

    day6 - issue with final function for part 2

--- day6_guard_gallivant/main.py
+++ day6_guard_gallivant/main.py
@@ -4,6 +4,40 @@ def count_distinct_positions(map_:list[list[str]]) -> int:
     Return the number of distinct positions the guard crosses
     before leaving the map.
     """
+    return len(get_distinct_positions(map_))
+
+def count_loop_obstructions(map_:list[list[str]]) -> int:
+    """
+    Return the number of positions which when obstructed, forces
+    the guard into a loop path.
+    """
+    possible_positions = get_distinct_positions(map_)
+    print(possible_positions)
+    current_pos = find_current_pos_and_dir(map_)[0]
+    possible_positions.discard(current_pos)
+
+    print(current_pos)
+    print(possible_positions)
+
+    count = 0
+    for position in possible_positions:
+        print("\n")
+        print("Analyzing position:", position)
+        # new_map = map_.copy()
+        # new_map[position[0]][position[1]] = "#"
+        if path_becomes_loop(map_, position):
+            count += 1
+            print(f"Found position {position}: {count}")
+        else:
+            print(f"Not a position {position}: {count}")
+    
+    return count
+
+def get_distinct_positions(map_:list[list[str]]) -> set[tuple[int, int]]:
+    """
+    Return the distinct positions the guard crosses before leaving
+    the map.
+    """
     rows, columns = len(map_), len(map_[0]) 
     distinct_positions = set()
 
@@ -21,7 +55,33 @@ def count_distinct_positions(map_:list[list[str]]) -> int:
         else:
             break
     
-    return len(distinct_positions)
+    return distinct_positions
+
+def path_becomes_loop(map_:list[list[str]], obstacle_pos:tuple[int, int]) -> bool:
+    """
+    Return True if the guard's path becomes a loop after `obstacle_pos` is
+    changed to "#".
+    """
+    map_[obstacle_pos[0]][obstacle_pos[1]] = '#'
+    rows, columns = len(map_), len(map_[0]) 
+    positions = []
+
+    current_pos, current_dir = find_current_pos_and_dir(map_)
+
+    while (0 <= current_pos[0] < rows) and (0 <= current_pos[1] < columns):
+        positions.append(current_pos)
+        next_pos = determine_next_possible_position(current_pos, current_dir)
+        if (0 <= next_pos[0] < rows) and (0 <= next_pos[1] < columns):
+            map_value = map_[next_pos[0]][next_pos[1]]
+            if map_value == "#":
+                current_dir = turn_right_90_deg(current_dir)
+            else:
+                if (next_pos in positions) and next_pos_is_obstacle(map_, next_pos, current_dir):
+                    return True
+                else:
+                    current_pos = next_pos
+        else:
+            return False
 
 def find_current_pos_and_dir(map_:list[list[str]]) -> tuple[tuple[int, int], str]:
     """
@@ -67,6 +127,15 @@ def turn_right_90_deg(current_dir:str) -> str:
         return '<'
     elif current_dir == '<':
         return '^'
+
+def next_pos_is_obstacle(map_:list[list[str]], position:tuple[int, int], direction:str) -> bool:
+    """
+    Return True if the next position is "#".
+    """
+    next_pos = determine_next_possible_position(position, direction)
+    if map_[next_pos[0]][next_pos[1]] == "#":
+        return True
+    return False
 
 def render_input() -> list[list[str]]:
     """
--- day6_guard_gallivant/test.py
+++ day6_guard_gallivant/test.py
@@ -1,21 +1,25 @@
 import unittest
 from main import (
+    count_loop_obstructions,
     count_distinct_positions,
+    get_distinct_positions,
+    path_becomes_loop,
     find_current_pos_and_dir,
     determine_next_possible_position,
     turn_right_90_deg,
+    next_pos_is_obstacle
 )
 
 class TestGuardGallivant(unittest.TestCase):
     def setUp(self):
         self.MAP1 = [
             list("....#....."),
-            list("....^....#"),
+            list(".........#"),
             list(".........."),
             list("..#......."),
             list(".......#.."),
             list(".........."),
-            list(".#........"),
+            list(".#..^....."),
             list("........#."),
             list("#........."),
             list("......#...")
@@ -40,28 +44,171 @@ class TestGuardGallivant(unittest.TestCase):
             list(">..#")
         ]
 
-    def test_distinct_positions_1(self):
+        # self.map1_77 = self.MAP1.copy()
+        # self.map1_77[7][7] = "#"
+
+        # self.map1_23 = self.MAP1.copy()
+        # self.map1_23[2][3] = "#"
+
+        # (7, 7)
+        self.MAP4 = [
+            list("....#....."),
+            list(".........#"),
+            list(".........."),
+            list("..#......."),
+            list(".......#.."),
+            list(".........."),
+            list(".#..^....."),
+            list(".......##."),
+            list("#........."),
+            list("......#...")
+        ]
+
+        # (2, 3)
+        self.MAP5 = [
+            list("....#....."),
+            list(".........#"),
+            list("...#......"),
+            list("..#......."),
+            list(".......#.."),
+            list(".........."),
+            list(".#..^....."),
+            list("........#."),
+            list("#........."),
+            list("......#...")
+        ]
+
+        # (7, 6)
+        self.MAP6 = [
+            list("....#....."),
+            list(".........#"),
+            list(".........."),
+            list("..#......."),
+            list(".......#.."),
+            list(".........."),
+            list(".#..^....."),
+            list("......#.#."),
+            list("#........."),
+            list("......#...")
+        ]
+
+        # (4, 4)
+        self.MAP7 = [
+            list("....#....."),
+            list(".........#"),
+            list(".........."),
+            list("..#......."),
+            list("....#..#.."),
+            list(".........."),
+            list(".#..^....."),
+            list("........#."),
+            list("#........."),
+            list("......#...")
+        ]
+
+        # (6, 3)
+        self.MAP8 = [
+            list("....#....."),
+            list(".........#"),
+            list(".........."),
+            list("..#......."),
+            list(".......#.."),
+            list(".........."),
+            list(".#.#^....."),
+            list("........#."),
+            list("#........."),
+            list("......#...")
+        ]
+
+    # def test_count_loop_obstructions_1(self):
+    #     self.assertEqual(
+    #         count_loop_obstructions(self.MAP1),
+    #         6
+    #     )
+
+    # def test_count_loop_obstructions_2(self):
+    #     self.assertEqual(
+    #         count_loop_obstructions(self.MAP2),
+    #         0
+    #     )
+
+    # def test_count_loop_obstructions_3(self):
+    #     self.assertEqual(
+    #         count_loop_obstructions(self.MAP3),
+    #         0
+    #     )
+    
+    def test_count_distinct_positions_1(self):
         self.assertEqual(
             count_distinct_positions(self.MAP1),
-            38
+            41
         )
 
-    def test_distinct_positions_2(self):
+    def test_count_distinct_positions_2(self):
         self.assertEqual(
             count_distinct_positions(self.MAP2),
             3
         )
 
-    def test_distinct_positions_3(self):
+    def test_count_distinct_positions_3(self):
         self.assertEqual(
             count_distinct_positions(self.MAP3),
             3
         )
 
+    def test_get_distinct_positions_1(self):
+        self.assertEqual(
+            get_distinct_positions(self.MAP1),
+            {(3, 4), (4, 3), (5, 4), (4, 6), (8, 3), (8, 6),
+             (1, 6), (2, 8), (7, 4), (6, 2), (7, 1), (7, 7),
+             (6, 5), (6, 8), (4, 2), (4, 5), (5, 6), (4, 8),
+             (8, 2), (9, 7), (8, 5), (2, 4), (1, 5), (1, 8),
+             (6, 4), (7, 3), (6, 7), (7, 6), (5, 2), (4, 4),
+             (3, 8), (8, 4), (5, 8), (8, 1), (8, 7), (1, 4),
+             (1, 7), (7, 2), (6, 6), (7, 5), (6, 3)}
+        )
+
+    def test_get_distinct_positions_2(self):
+        self.assertEqual(
+            get_distinct_positions(self.MAP2),
+            {(9, 5), (9, 3), (9, 4)}
+        )
+
+    def test_get_distinct_positions_3(self):
+        self.assertEqual(
+            get_distinct_positions(self.MAP3),
+            {(2, 0), (2, 1), (2, 2)}
+        )
+
+    def test_path_becomes_loop_1(self):
+        self.assertTrue(
+            path_becomes_loop(self.MAP1, (7, 7))
+        )
+
+    def test_path_becomes_loop_2(self):
+        self.assertFalse(
+            path_becomes_loop(self.MAP1, (2, 3))
+        )
+
+    def test_path_becomes_loop_3(self):
+        self.assertTrue(
+            path_becomes_loop(self.MAP1, (7, 6))
+        )
+
+    def test_path_becomes_loop_4(self):
+        self.assertFalse(
+            path_becomes_loop(self.MAP1, (4, 4))
+        )
+
+    def test_path_becomes_loop_5(self):
+        self.assertTrue(
+            path_becomes_loop(self.MAP1, (6, 3))
+        )
+
     def test_find_current_pos_and_dir_1(self):
         self.assertEqual(
             find_current_pos_and_dir(self.MAP1),
-            ((1, 4), "^")
+            ((6, 4), "^")
         )
 
     def test_find_current_pos_and_dir_2(self):
@@ -124,6 +271,21 @@ class TestGuardGallivant(unittest.TestCase):
         self.assertEqual(
             turn_right_90_deg('<'),
             '^'
+        )
+
+    def test_next_pos_is_obstacle_1(self):
+        self.assertTrue(
+            next_pos_is_obstacle(self.MAP1, (3, 1), '>')
+        )
+
+    def test_next_pos_is_obstacle_2(self):
+        self.assertFalse(
+            next_pos_is_obstacle(self.MAP1, (5, 4), '^')
+        )
+
+    def test_next_pos_is_obstacle_3(self):
+        self.assertTrue(
+            next_pos_is_obstacle(self.MAP1, (4, 8), '<')
         )
 
 
